90017407f568059b6f03ed023408961859028187
Enzo <14167738+ntilau@users.noreply.github.com>
2026-08-28 12:55:40 +0200

    Update user-agent.sh

--- user-agent.sh
+++ user-agent.sh
@@ -2,3 +2,5 @@
 USER_AGENT='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36 Edg/147.0.0.0'
 echo defaults write com.apple.Safari CustomUserAgent \'$USER_AGENT\'
 defaults write com.apple.Safari CustomUserAgent \'$USER_AGENT\'
+# defaults delete com.apple.Safari CustomUserAgent
+
